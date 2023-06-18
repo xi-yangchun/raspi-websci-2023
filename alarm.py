cac11e6ba5ede489ec79483727368f53ab302919
xi-yangchun <ittetsu1945@gmail.com>
2023-06-19 04:37:04 +0900

    added simple lightalarm

---
+++ alarm.py
@@ -0,0 +1,43 @@
+import datetime
+import time
+import serial
+
+class alarmclock:
+    def __init__(self):
+        self.targ_hour=0
+        self.targ_min=0
+        self.now_hour=0
+        self.now_min=0
+        self.sensing=False
+        self.ring_duration=5
+        # Replace '/dev/cu.usbmodem1101' with your Arduino's serial port
+        self.ser = serial.Serial('/dev/cu.usbmodem1401', 9600)
+        self.th_0=30
+    
+    def get_cds_line(self):
+        if self.ser.in_waiting > 0:
+            line = self.ser.readline().decode('utf-8').rstrip()
+            return line
+        else:
+            return '0'
+    
+    def run_alarm_under_lux(self):
+        self.targ_hour=1
+        self.targ_min=39
+        # set target datetime of alarming
+        dt_targ=datetime.datetime.now()
+        dt_targ=dt_targ.replace(hour=self.targ_hour,
+                minute=self.targ_min,second=0,microsecond=0)
+        while(1):
+            time.sleep(0.1)
+            dt_now=datetime.datetime.now()
+            delta=(dt_targ-dt_now)
+            if delta.total_seconds()<=0 and delta.total_seconds()>=-self.ring_duration*60:
+                if int(self.get_cds_line())<=self.th_0:
+                    print('ring!!!! {}'.format(delta.total_seconds()))
+    
+    def make_sound(self):
+        pass
+
+AC=alarmclock()
+AC.run_alarm_under_lux()
