0a0489244357bc103a57a2aad4d44df772b5e671
ciaran-regan-ie <ciaranregan123@gmail.com>
2023-06-22 16:03:12 +0900

    Update Client

--- client/client.py
+++ client/client.py
@@ -3,37 +3,34 @@ import serial
 from time import sleep
 
 def postServer(lightOut: str):
-    url = 'locallhost:3000/'
+    url = 'http://192.168.1.70:5000'
 
     data = {
         'lightOut': lightOut,
     }
 
     try:
-        # Send a POST request to the server
-        response = requests.post(url, json=data)
+        response = requests.get(url, json=data)
 
         # Check the response status code
         if response.status_code == requests.codes.ok:
             # Request was successful
-            print('POST request successful')
-            # Parse the response as JSON
-            response_data = response.json()
-            # Process the response data as needed
-            print('Response:', response_data)
+            print('get request successful')
         else:
             # Request failed
-            print('POST request failed with status code:', response.status_code)
+            print('get request failed with status code:', response.status_code)
 
     except requests.exceptions.RequestException as e:
         # Request encountered an exception
-        print('POST request failed:', e)
+        print('get request failed:', e)
 
-    return None 
-serOut = serial.Serial('/dev/cu.usbmodem1201', 9601)  # Replace '/dev/cu.usbmodem1101' with your Arduino's serial port
+    return None
+
+serOut = serial.Serial('/dev/tty.usbmodem21101', 9601)  # Replace '/dev/cu.usbmodem1101' with your Arduino's serial port
 
 while True:
     if serOut.in_waiting > 0:
         lightOut = serOut.readline().decode('utf-8').rstrip()
+        print(lightOut)
         postServer(lightOut)
-        sleep(10) # Check the light value every 10 seconds
+        sleep(5) # Check the light value every 10 seconds
